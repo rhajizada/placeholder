eb7521f2997de37138cbe777ed81e21a8ac28c19
Rufat Hajizada <hajizada.rufat@gmail.com>
2024-09-28 00:10:10 -0400

    Added code to remove previous keymap registrations

--- lua/placeholder/init.lua
+++ lua/placeholder/init.lua
@@ -1,16 +1,15 @@
 local M = {}
-
--- Import config and functionality from other modules
 local config = require("placeholder.config")
 local placeholder = require("placeholder.placeholder")
 
--- Setup function that allows user configuration
-function M.setup(user_config)
-	-- Set up user configurations in config.lua
-	config.setup(user_config)
-	placeholder.setup()
+-- Function to handle keymap registration
+local function set_keymap()
+	-- Remove the old keymap if one exists
+	if config.options.previous_keymap then
+		pcall(vim.keymap.del, "n", config.options.previous_keymap)
+	end
 
-	-- Set the keymap using the updated configuration
+	-- Set the new keymap
 	vim.keymap.set(
 		"n",
 		config.options.keymap,
@@ -18,13 +17,17 @@ function M.setup(user_config)
 		{ desc = "Add debug configuration" }
 	)
 
-	-- Register keybinding with which-key if it's installed
-	if pcall(require, "which-key") then
-		local wk = require("which-key")
-		wk.add({
-			[config.options.keymap] = { placeholder.add_debug_configuration, desc = "Add debug configuration" },
-		})
-	end
+	-- Update the previous_keymap to the new keymap
+	config.options.previous_keymap = config.options.keymap
+end
+
+function M.setup(user_config)
+	-- Set up user configurations in config.lua
+	config.setup(user_config)
+	placeholder.setup()
+
+	-- Set the keymap
+	set_keymap()
 end
 
 -- Function to add the debug configuration
